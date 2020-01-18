99bb647d9d8db7ba9c34d3f1a52c92c39c3dab79
Verb <58372226+Verb178@users.noreply.github.com>
2020-01-18 20:17:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ class Verbroulette {
 			await this.channel.send(`${this.prefix}kick ${player} ${description}`);
 			await this.channel.send({embed: this.embedKick(player.user, description)});
 			await this.channel.createInvite({maxAge: 0}).then(invite => {
-			await player.user.send(`**Voici ton lien d'invitation**: ${invite}`);
+			player.user.send(`**Voici ton lien d'invitation**: ${invite}`);
 			return player.kick(player, description);
 		})
 	}
