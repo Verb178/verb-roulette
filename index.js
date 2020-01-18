640b0792294c14d45822a82df262575daa57abb3
Verb <58372226+Verb178@users.noreply.github.com>
2020-01-18 18:50:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -177,8 +177,8 @@ class HorsengelRoulette {
 		if (this.bot.hasPermission('KICK_MEMBERS')) {
 			await this.channel.send(`${this.prefix}kick ${player} ${description}`);
 			await this.channel.send({embed: this.embedKick(player.user, description)});
-			this.channel.createInvite({maxAge: 0}).then(invite => {
-			await player.user.send(`**Voici ton lien d'invitation**: ${invite}`);
+			awaitthis.channel.createInvite({maxAge: 0}).then(invite => {
+			player.user.send(`**Voici ton lien d'invitation**: ${invite}`);
 			return player.kick(player, description);
 		})
 	}
